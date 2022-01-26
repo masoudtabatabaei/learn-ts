3ed43d533291f567977559cc34b69615d510f0a2
Masoud Tabatabaei <masoudtabatabaei20@gmail.com>
2022-01-26 14:25:03 +0330

    work with union types

--- index.js
+++ index.js
@@ -13,3 +13,28 @@ var resultPhrase = 'Result is: ';
 number1 = 8;
 var result = add(number1, number2, printResult, resultPhrase);
 console.log(result);
+var person = {
+    name: "masoud",
+    age: 30,
+    role: ["author", 4]
+};
+console.log(person.role);
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["AUTHOR"] = 1] = "AUTHOR";
+    Role[Role["OPERATOR"] = 2] = "OPERATOR";
+})(Role || (Role = {}));
+;
+console.log(Role.AUTHOR);
+function concatenate(input1, input2) {
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        return input1 + input2;
+    }
+    else {
+        return input1.toString() + " " + input2.toString();
+    }
+}
+console.log(concatenate(2, 8));
+console.log(concatenate("salam", "masoud"));
+console.log(concatenate(2, "numbers"));
--- index.ts
+++ index.ts
@@ -15,3 +15,34 @@ number1 = 8;
 
 const result = add(number1 , number2 , printResult , resultPhrase);
 console.log(result);
+
+
+
+let person : {
+  name: string ,
+  age: number ,
+  role: [string , number]
+} = {
+  name : "masoud" ,
+  age : 30 ,
+  role : ["author" , 4]
+};
+
+console.log(person.role);
+
+enum Role {ADMIN  , AUTHOR , OPERATOR};
+console.log(Role.AUTHOR);
+
+
+function concatenate(input1: number|string , input2: number|string) {
+  if(typeof input1 === "number" && typeof input2 === "number") {
+    return input1 + input2;
+  } else {
+    return input1.toString() + " " + input2.toString();
+  }
+}
+
+
+console.log(concatenate(2,8));
+console.log(concatenate("salam" , "masoud"));
+console.log(concatenate(2 , "numbers"));
