52833c4ede70a51e89854b82bd7c94fbcbadf5d3
Masoud Tabatabaei <masoudtabatabaei20@gmail.com>
2022-01-30 14:00:23 +0330

    Training: test diffrent scenarios for learning generics

--- generics.ts
+++ generics.ts
@@ -1,35 +1,81 @@
-function identity<T>(arg:T):T {
-  return arg;
+function fun<gen>(item:gen):gen {
+  return item;
 }
 
-let num = identity(2628);
-let str = identity("Hi");
+let first = fun<string>("Test name");
+let second = fun<number>(12345);
 
-// console.log(typeof num); // number
-// console.log(typeof str); // string
+// console.log(first , ` => is ${typeof first}`);
+// console.log(second , ` => is ${typeof second}`);
 
-class Queue<T> {
-  protected data: T[] = [];
+//--------------------------------------------------------------------
 
-  push(item: T) {
-    this.data.push(item);
+function fun1<T , U , Y>(a:T , b:U , c:Y):T {
+  return a;
+}
+
+let result = fun1("masoud" , 123 , true);
+
+// console.log(result);
+
+class CustomArray<T> {
+  private arr:T[] = [];
+
+  getItems() {
+    return this.arr;
   }
 
-  pop(): T | undefined {
-    return this.data.shift();
+  addItem(item:T) {
+    return this.arr.push(item);
+  }
+
+  removeItem(item:T) {
+    let index = this.arr.indexOf(item);
+    if (index > -1) {
+      return this.arr.splice(index , 1);
+    }
   }
 }
 
-let q = new Queue<number>();
+let customArray = new CustomArray<number | string>();
 
-q.push(1);
-q.push(2);
-q.push(3);
-q.push(4);
+customArray.addItem(1234);
+customArray.addItem("Aaa");
+customArray.addItem(9101112);
 
-console.log(q);
+// customArray.removeItem("Aaa");
 
-console.log(q.pop().toPrecision());
-console.log(q.pop().toPrecision()); // toPrecision is not a function
-console.log(q.pop().toPrecision());
-console.log(q.pop().toPrecision());
+// console.log(customArray.getItems());
+// console.log(customArray);
+
+//---------------------------------------------------------------
+interface funcArgs {
+  length: number;
+}
+
+function getLength<T extends funcArgs>(args: T) : number {
+  return args.length;
+}
+
+console.log(getLength("masoudsdfsdfdsf"));
+console.log(getLength({ length: 5, name: 'Hello'}));
+// console.log(getLength(true));
+
+// -------------------------------------------------------------------------
+
+class Person {
+  firstName: string;
+  lastName: string;
+
+  constructor(fname:string,  lname:string) {
+    this.firstName = fname;
+    this.lastName = lname;
+  }
+}
+
+function display<T extends Person>(pers:T): void {
+  console.log(pers.firstName + " " + pers.lastName);
+}
+
+console.log(display({firstName:"mas" , lastName:"oud"}))
+console.log(display(new Person("Masoud" , "Tabatabaei")));
