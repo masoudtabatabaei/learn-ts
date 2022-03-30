f16e4298f2a09f977784d03f7a4c7d95469c27b1
Masoud <masoudtabatabaei20@gmail.com>
2022-03-30 15:13:56 +0430

    Initial tsconfig file

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,18 +1,1 @@
-const mixed: (string|number)[] = [];
-
-mixed.push(1234);
-mixed.push('Ahmad');
-mixed.push(25);
-
-// console.log(mixed);
-
-let obj1 : {
-    name?: string ,
-    age?: number ,
-    color?: string
-};
-
-obj1 = {name:"ali"};
-obj1 = {age:20 , color: "blue"};
-
-console.log(obj1);
+console.log('TEST');
