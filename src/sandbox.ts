a021fe5f8eeb99c8e22468ceb17a9a38a993eb01
Masoud <masoudtabatabaei20@gmail.com>
2022-03-30 14:43:44 +0430

    Change project architecture

---
+++ src/sandbox.ts
@@ -0,0 +1,18 @@
+const mixed: (string|number)[] = [];
+
+mixed.push(1234);
+mixed.push('Ahmad');
+mixed.push(25);
+
+// console.log(mixed);
+
+let obj1 : {
+    name?: string ,
+    age?: number ,
+    color?: string
+};
+
+obj1 = {name:"ali"};
+obj1 = {age:20 , color: "blue"};
+
+console.log(obj1);
